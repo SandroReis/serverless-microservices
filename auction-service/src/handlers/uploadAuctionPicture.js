55d0cb60805e79094b74c3671bcb979df0715543
Sandro Reis <sandroreis@hotmail.com>
2021-05-19 10:24:21 -0300

    feat: add treatment to upload auction picture

--- auction-service/src/handlers/uploadAuctionPicture.js
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -1,8 +1,29 @@
+import { getAuctionById } from './getAuction'
+import { uploadPictureToS3 } from '../lib/uploadPictureToS3'
+import middy from '@middy/core'
+import httpErrorHandler from '@middy/http-error-handler'
+import createError from 'http-errors'
+
 export async function uploadAuctionPicture(event) {
-  return{
-    statusCode: 200,
-    body: JSON.stringify({})
+  const { id } = event.pathParameters
+  const auction = await getAuctionById(id)
+  const base64 = event.body.replace(/^data:image\/\w+;base64,/, '')
+  const buffer = Buffer.from(base64, 'base64')
+
+  try {
+    const uploadToS3Result = await uploadPictureToS3(auction.id + '.jpg', buffer)
+    console.log(uploadToS3Result)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({})
+    }
+  } catch (error) {
+    console.error(error)
+    throw new createError.InternalServerError(error)
   }
+
 }
 
-export const handler = uploadAuctionPicture
+export const handler = middy(uploadAuctionPicture)
+  .use(httpErrorHandler)
